8379c9806c06546132eeb733f86aef94c2c3167e
jwhite37 <jwhite37@ur.rochester.edu>
2015-11-18 11:53:01 -0500

    Sentiment Analysis
    Word Tag Counts with Sentiment for the multi_user_3 is done!

--- app/sentiment_analysis.py
+++ app/sentiment_analysis.py
@@ -26,21 +26,55 @@ from textblob.sentiments import NaiveBayesAnalyzer
 pp = pprint.PrettyPrinter(indent=2)
 
 #=======================================================
-# Processing of a users frequent words for running
-# through the sentiment analysis.
+# Loading frequent word counts from file and removal
+# of stopwords.
 #=======================================================
 def process_user_freq(filepath):
     formatted_data = []
+
+    stopwords = ["the","be","to","of","and","a","in","that",
+                 "have","it","for","not","on","with","he",
+                 "as","do","at","this","but","his","by",
+                 "from","they","we","say","her","she","or",
+                 "an","will","my","all","would","there","their",
+                 "what","so","up","out","if","about","who","get",
+                 "which","go","me","when","make","can",
+                 "time","just","him","know","take","into","your",
+                 "some","could","them","see","other","than",
+                 "then","now","only","come","its","over",
+                 "think","also","back","after","use","how",
+                 "our","first","well","way","even","new",
+                 "because","any","these","give","most",
+                 "is","are","was","does","been","where","why",
+                 "am","being","thing","really","something","however",
+                 "said","got","such","it's","don't","that's",
+                 "what's","did","too","gotten"]
 
     with open(filepath,"r+") as the_file:
         loaded_data = json.loads(the_file.read())
         counts = loaded_data['counts']
 
         for e in counts:
-            sample = e['text']
-            formatted_data.append(sample)
+            if str.lower(str(e['text'])) not in stopwords:
+                formatted_data.append(e)
 
     return formatted_data
+
+#=======================================================
+# Aggregation of Users Frequent Words into one
+# set of information for the entire session.
+#=======================================================
+def aggregate_freq_words(list):
+    final_data = {}
+
+    for e in list:
+        for j in e:
+            if final_data.has_key(j['text']):
+                final_data[j['text']] += final_data[j['text']] + j['count']
+            else:
+                final_data[j['text']] = j['count']
+
+    return final_data
 
 #=======================================================
 # Perform a read of a sliding window across a users
@@ -72,13 +106,41 @@ def sliding_window(filepath, length):
 # Main Function
 #=======================================================
 if __name__ == "__main__":
+
+    #-------------------------------------------------------------------------------------------------------------
+    # Getting Sentiment for the word counts for the entire session and write to file
+    #-------------------------------------------------------------------------------------------------------------
     basepath = os.path.dirname(__file__)
-    filepath = os.path.abspath(os.path.join(basepath, "session_data/multi_test_3/user_3/formatted-alignment.json"))
+    filepath_1 = os.path.abspath(os.path.join(basepath, "session_data/multi_test_3/user_1/word-tag-count.json"))
+    filepath_2 = os.path.abspath(os.path.join(basepath, "session_data/multi_test_3/user_2/word-tag-count.json"))
+    filepath_3 = os.path.abspath(os.path.join(basepath, "session_data/multi_test_3/user_3/word-tag-count.json"))
+    filepath_4 = os.path.abspath(os.path.join(basepath, "session_data/multi_test_3/user_4/word-tag-count.json"))
 
-    data = process_user_freq(filepath)
+    session_counts = []
+    session_counts.append(process_user_freq(filepath_1))
+    session_counts.append(process_user_freq(filepath_2))
+    session_counts.append(process_user_freq(filepath_3))
+    session_counts.append(process_user_freq(filepath_4))
+   
+    processed_counts = aggregate_freq_words(session_counts)
 
-    #data = sliding_window(filepath, 5)
-    
+    list_data = []
 
-    #pp.pprint(data)
-  
+    for e,v in processed_counts.iteritems():
+        blob = TextBlob(e)
+        sentiment = blob.sentiment
+
+        dict = {"text": e, "counts": v, "sentiment": sentiment[0]}
+        list_data.append(dict)
+
+    data_to_write = {"counts": list_data}
+
+    filepath_final = os.path.abspath(os.path.join(basepath, "session_data/multi_test_3/sentiment_counts.json"))
+
+    with open(filepath_final,'w') as final_file:
+        final_file.write(pp.pformat(data_to_write))
+
+    #-------------------------------------------------------------------------------------------------------------
+    # Obtaining a graph of sentiment for each users and depositing that into their directory
+    #-------------------------------------------------------------------------------------------------------------
+
