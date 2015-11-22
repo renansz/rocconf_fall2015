dce245c60fb933d24563a306f0d2d039da799382
Yue Wang <yuewang@dhcp-10-5-4-100.wireless.rochester.edu>
2015-11-22 02:09:46 -0500

    hope I don't need to modify anymore, should be all set now

--- app/transition_analysis.py
+++ app/transition_analysis.py
@@ -19,8 +19,8 @@ def read_matrix(filepath):
         i+=1;
     return data
 
-#read session_data, label user
-def read_list(filepath,number_user,list):
+#read session_data from word-prosody.json, label user
+def read_list_wordprosody(filepath,number_user,list):
     
     file=open(filepath,"r+")
     loaded_data=json.loads(file.read())
@@ -29,6 +29,19 @@ def read_list(filepath,number_user,list):
         if(time_stamp['word'] !='sp'):
             list.append((time_stamp['start_time_secs'], number_user))
     return list
+
+#read session_data from formatted-alignment.json, label user
+
+def read_list_formattedalignment(filepath,number_user,list):
+    
+    file=open(filepath,"r+")
+    loaded_data=json.loads(file.read())
+    
+    for time_stamp in loaded_data['phone']:
+        if(time_stamp['speech'] !='sp'):
+            list.append((time_stamp['startTime'], number_user))
+    return list
+
 
 #smooth method
 def smooth_list(list,iteration):
@@ -49,16 +62,54 @@ def smooth_list(list,iteration):
 def smooth_call(list):
     
     while 1:
+        
         list=smooth_list(list,1)
         list=smooth_list(list,2)
 
         temp=smooth_list(list,1)
-        temp=smooth_list(temp,1)
+        temp=smooth_list(temp,2)
 
         if(temp==list):
             break
         list=temp
+
     return temp
+# remove duplicate start-time, further smooth noisy data
+def remove_duplicate(list):
+    out=[]
+    for i in range(0,len(list)-1):
+        if(list[i][0]!=list[i+1][0]):
+            out.append((list[i][0],list[i][1]))
+    j=3
+    while j<len(out)-4:
+#        print out[j-3][1], out[j][1],out[j+3][1]
+        if(out[j][1]!=out[j-3][1] and out[j][1]!=out[j+3][1]):
+            out.pop(j)
+        j=j+1
+
+    return out
+# iterate smoothing methods to get stablized matrix
+def iterate_matrix(list,number_user):
+    while 1:
+    
+        list=smooth_call(list)
+        list=remove_duplicate(list)
+        list=smooth_call(list)
+        matrix_first=transition_matrix(number_user,list)
+
+        list=smooth_call(list)
+        list=remove_duplicate(list)
+        list=smooth_call(list)
+        matrix_second=transition_matrix(number_user,list)
+
+        if(matrix_first.all()==matrix_second.all()):
+            break
+    pp.pprint(list)
+    
+    return matrix_second
+
+
+
 
 #calculate transition matrix
 def transition_matrix(number_user,list):
@@ -80,17 +131,21 @@ def transition_matrix(number_user,list):
 
 if __name__=='__main__':
     
-#    read in file
+
     list=[]
     for i in range(1,4):
-        list=read_list('/Users/yuewang/Desktop/rocconf_fall2015/app/session_data/multi_test_2/user_'+ str(i) +'/word-prosody.json',i,list)
-#    smooth data to remove noise 
-    list=smooth_call(sorted(list))
-    pp.pprint(list)
-#    calculate transition matrix
-    matrix=transition_matrix(3,list)
+        list=read_list_wordprosody('/Users/yuewang/Desktop/rocconf_fall2015/app/session_data/multi_test_2/user_'+ str(i) +'/word-prosody.json',i,list)
+
+    matrix=iterate_matrix(sorted(list),3)
     pp.pprint(matrix)
 
+
+    list=[]
+    for i in range(1,4):
+        list=read_list_formattedalignment('/Users/yuewang/Desktop/rocconf_fall2015/app/session_data/multi_test_2/user_'+ str(i) +'/formatted-alignment.json',i,list)
+
+    matrix=iterate_matrix(sorted(list),3)
+    pp.pprint(matrix)
 
 
 
