050f4600fb20a325ceb47d9d52882ce8afe151b3
Yue Wang <yuewang@Yues-MacBook-Air.local>
2015-12-03 14:39:02 -0500

    smile analysis modified

--- app/session_analysis.py
+++ app/session_analysis.py
@@ -565,54 +565,61 @@ def generate_smile_counts(session):
 
     print "Smile counts generated..."
 
-def read_matrix(filepath):
-    
-    file=open(filepath)
-    featuredict=json.loads(file.read())
-    data=np.zeros((len(featuredict['features']),len(featuredict['features'][0])))
-    i=0
-    for time_stamp in featuredict['features']:
-        
-        data[i,:]=time_stamp.values()
-        i+=1;
-    return data
 
-
-def read_list_smile(filepath):
-    list = []
-    file = open(filepath,"r+")
-    loaded_data = json.loads(file.read())
+    def read_list_smile(filepath):
+    list=[]
+    file=open(filepath,"r+")
+    loaded_data=json.loads(file.read())
     
     for time_stamp in loaded_data['features']:
-        list.append((time_stamp['time_millisec'],time_stamp['smile_cubicSpline']))
+        list.append((time_stamp['time_millisec'], time_stamp['smile_cubicSpline']))
     return list
 
 
-def read_list_average(filepath):
-    file=open(filepath,"r+")
-    loaded_data=json.loads(file.read())
-    return (loaded_data['features'][0])['averageSmileIntensity_100']
+
+def convert_list_smile(filepath):
+    list_millisec=read_list_smile(filepath)
+    list_second=[]
+    max=0
+    for items in list_millisec:
+        if(items[1]>max):
+            max=items[1]
+        if(items[0]%2000==0):
+            list_second.append((items[0]/2000,max))
+            max=0
+    return list_second
+
+
+def getthreadhold(filepath):
+    data=convert_list_smile(filepath)
+    max=0
+    list=[]
+    for i in data:
+        list.append(i[1])
+        if(i[1]>max):
+            max=i[1]
+    return (max-np.std(list))
 
 
 def smile_count(user_number, session):
     count={}
-    for i in range(1, user_number+1):
+    for i in range(1,user_number+1):
         basepath = os.path.dirname(__file__)
         filepath1 = os.path.abspath(os.path.join(basepath, "session_data/" + session + "/user_" + str(i) + "/audio-video-features.json"))
-        filepath2 = os.path.abspath(os.path.join(basepath, "session_data/" + session + "/user_" + str(i) + "/average-features.json"))
 
-        data = read_list_smile(filepath1)
-        ave = read_list_average(filepath2)
-        
-        for j in range(0,len(data)):
-            if(count.has_key(data[j][0])):
-                if(data[j][1]>ave):
-                    count[data[j][0]]=count[data[j][0]]+1
+        data_millisec=read_list_smile(filepath1)
+        data_second=convert_list_smile(filepath1))
+        threadhold=getthreadhold(filepath1))
+
+        for items in data_second:
+            if(count.has_key(items[0])):
+                if(items[1]>threadhold):
+                    count[items[0]]=count[items[0]]+1
             else:
-                if(data[j][1]>ave):
-                    count[data[j][0]]=1
+                if(items[1]>threadhold):
+                    count[items[0]]=1
                 else:
-                    count[data[j][0]]=0
+                    count[items[0]]=0
     return count
 
 #=======================================================
